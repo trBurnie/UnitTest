b482d0eae9f0f0b4bd81ff836411a925f91a8291
sergelse <sergelse@ROSA03.ad.arcada.fi>
2015-02-09 12:09:28 +0200

    Created MyInterface.java Created a Testfunktions useing assertTrue

--- Lecture8/Test/MyTestClass.java
+++ Lecture8/Test/MyTestClass.java
@@ -1,10 +1,18 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 
 public class MyTestClass {
+
 	@Test
 	public void readFromKeyboard_none_valid() {
 		//I don't know how to make this test
+		
+		//noob, this is how you do it
+
+		MyClass myclass = new MyClass(null);
+		assertTrue(myclass.text.equals("test"));
 	}
 	
 	@Test
